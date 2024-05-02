71125215850a2c33cee482345a0355471938d732
pro-masud <promasudbd@gmail.com>
2024-05-02 12:24:01 +0600

    woocommerce and elementor addon development plugin testing

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Elementor Addon
+ * Description: Simple hello world widgets for Elementor.
+ * Version:     1.0.0
+ * Author:      Elementor Developer
+ * Author URI:  https://developers.elementor.com/
+ * Text Domain: elementor-addon
+ *
+ * Requires Plugins: elementor
+ * Elementor tested up to: 3.21.0
+ * Elementor Pro tested up to: 3.21.0
+ */
+
+function register_hello_world_widget( $widgets_manager ) {
+
+	require_once( __DIR__ . '/widgets/widget-1.php' );
+
+	$widgets_manager->register( new \Elementor_Hello_World_Widget_1() );
+
+}
+add_action( 'elementor/widgets/register', 'register_hello_world_widget' );
